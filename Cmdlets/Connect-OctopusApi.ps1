2e1c39feb757c94d35c3ee57fb0b3899f221c952
Paul Marston <paul@marston.me>
2017-05-08 12:52:58 +0100

    ds

--- Cmdlets/Connect-OctopusApi.ps1
+++ Cmdlets/Connect-OctopusApi.ps1
@@ -1,22 +1,29 @@
 function Connect-OctopusApi {
     param(
-        [Parameter(Mandatory)]$Uri,
-        [Parameter(Mandatory)]$ApiKey
+        [Parameter(ParameterSetName = 'Module',Mandatory)]$Uri,
+        [Parameter(ParameterSetName = 'Module',Mandatory)]$ApiKey,
+        [Parameter(ParameterSetName = 'CloudStorage',Mandatory)][switch]$CloudStorage
     )
     
     $success = $false
     try {
+        if ($CloudStorage) {
+            $ExecutionContext.SessionState.Module.PrivateData['OctopusApi'] = Get-CloudStorage OctopusUtilities -Local
+        } else {
+            $baseUri = $Uri.Trim('/')
+            if ($baseUri.EndsWith('/api', [System.StringComparison]::OrdinalIgnoreCase)) { $baseUri = $baseUri.Remove($baseUri.Length-3, 3).Trim('/') }
 
-        $baseUri = $Uri.Trim('/')
-        if ($baseUri.EndsWith('/api', [System.StringComparison]::OrdinalIgnoreCase)) { $baseUri = $baseUri.Remove($baseUri.Length-3, 3).Trim('/') }
-
-        $ExecutionContext.SessionState.Module.PrivateData['OctopusApi'] = @{
-            BaseUri = $baseUri
-            ApiKey = $ApiKey
+            $ExecutionContext.SessionState.Module.PrivateData['OctopusApi'] = @{
+                BaseUri = $baseUri
+                ApiKey = $ApiKey
+            }
         }
-
         if (Invoke-OctopusApi '/' | ? Application -eq "Octopus Deploy") {
             Write-Host -ForegroundColor Green "Connection successful."
+        }
+
+        if (Get-Module CloudStorage) {
+            Set-CloudStorage OctopusUtilities -Local $ExecutionContext.SessionState.Module.PrivateData['OctopusApi']
         }
     }
     catch {
